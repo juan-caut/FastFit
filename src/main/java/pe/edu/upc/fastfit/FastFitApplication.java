d8721e4634e31898b829b0ae7935a25507e2f1a8
juan c <82497625+juan-caut@users.noreply.github.com>
2023-05-23 17:53:46 -0500

    project base

---
+++ src/main/java/pe/edu/upc/fastfit/FastFitApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.fastfit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FastFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FastFitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/upc/fastfit/FastFitApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.upc.fastfit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastFitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
