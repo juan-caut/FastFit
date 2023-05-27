0a8aafa7513745284dc2a4f1016230d031be3b1b
juan c <82497625+juan-caut@users.noreply.github.com>
2023-05-27 14:22:33 -0500

    add review service

---
+++ src/main/java/pe/edu/upc/fastfit/services/IReviewService.java
@@ -0,0 +1,12 @@
+package pe.edu.upc.fastfit.services;
+
+import pe.edu.upc.fastfit.entities.Review;
+
+import java.util.List;
+
+public interface IReviewService {
+    public void insert(Review review);
+    List<Review> list();
+    public void delete(int idReview);
+    public Review listId(int idReview);
+}
