c241e2ff00568907bebda04be29a61e2940c0d1a
juan c <82497625+juan-caut@users.noreply.github.com>
2023-05-28 01:39:14 -0500

    add turn DTO

--- src/main/java/pe/edu/upc/fastfit/dtos/TurnDTO.java
+++ src/main/java/pe/edu/upc/fastfit/dtos/TurnDTO.java
@@ -1,9 +1,5 @@
 package pe.edu.upc.fastfit.dtos;
 
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import java.time.LocalDate;
 
 public class TurnDTO {
@@ -13,4 +9,44 @@ public class TurnDTO {
     private int duration;
     private int TurnStatus_id;
     private int Psychologist_id;
+
+    public int getIdTurn() {
+        return idTurn;
+    }
+
+    public void setIdTurn(int idTurn) {
+        this.idTurn = idTurn;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public int getTurnStatus_id() {
+        return TurnStatus_id;
+    }
+
+    public void setTurnStatus_id(int turnStatus_id) {
+        TurnStatus_id = turnStatus_id;
+    }
+
+    public int getPsychologist_id() {
+        return Psychologist_id;
+    }
+
+    public void setPsychologist_id(int psychologist_id) {
+        Psychologist_id = psychologist_id;
+    }
 }
