eafd93c6a7cc4514d64e5000d1fd143ba179f70d
Rasec2 <109819663+Rasec2@users.noreply.github.com>
2023-06-12 02:07:55 -0500

    Se agregaron los queries de la entidad client

--- src/main/java/pe/edu/upc/fastfit/repositories/IClientRepository.java
+++ src/main/java/pe/edu/upc/fastfit/repositories/IClientRepository.java
@@ -1,8 +1,20 @@
 package pe.edu.upc.fastfit.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import pe.edu.upc.fastfit.entities.Client;
+
+import java.util.List;
+
 @Repository
 public interface IClientRepository extends JpaRepository<Client,Integer>{
+
+    @Query("from Client c where c.age between :min and :max")
+    List<Client> findByAge(@Param("min") int min, @Param("max") int max);
+
+    @Query("from Client c where c.age=:name")
+    List<Client> findByName(@Param("name") String name);
+
 }
